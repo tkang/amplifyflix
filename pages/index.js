806e87025a44378062bb967ac068c88c86e680fc
Taeho Kang <tkang1@gmail.com>
2021-08-13 16:34:39 +0900

    load sample tmdb ids

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,27 @@
 import Head from "next/head";
+import SAMPLE_TMDB_IDS from "../src/sample_tmdb_ids";
+import { useEffect, useState } from "react";
+import _ from "lodash";
+
+const TMDB_API_KEY = "ca294fadd74fb6ddb4e74a12e521ceae";
+const TMDB_MOVIE_API_URL = "https://api.themoviedb.org/3/movie/";
+const TMDB_MOVIE_POSTER_PATH = "https://image.tmdb.org/t/p/original/";
+
+function generateTmdbMovieApiUrl(tmdbId) {
+  return `${TMDB_MOVIE_API_URL}${tmdbId}?api_key=${TMDB_API_KEY}`;
+}
+
+function generateMoviePosterUrl(posterPath) {
+  return `${TMDB_MOVIE_POSTER_PATH}${posterPath}`;
+}
+
+console.log(generateTmdbMovieApiUrl(SAMPLE_TMDB_IDS[0]));
 
 function Home() {
+  const [tmdbIds, setTmdbIds] = useState(_.shuffle(SAMPLE_TMDB_IDS));
+
+  useEffect(() => {}, []);
+
   return (
     <div>
       <Head>
